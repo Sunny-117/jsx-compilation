77270c5c4faae59460471753efbde932c0ce1f22
付志强 <zhiqiangfu6@gmail.com>
2023-01-18 18:09:49 +0800

    feat: playgound deploy

--- src/parser.ts
+++
@@ -1,4 +0,0 @@
-export function parser(tokens) {
-    return {}
-
-}
--- tests/parser.spec.ts
+++
@@ -1,8 +0,0 @@
-import { parser } from "../src/parser";
-describe("parser", () => {
-  it("parser tokens to ast", () => {
-    const tokens = []
-    const ast = {}
-    expect(parser(tokens)).toEqual(ast);
-  });
-});
