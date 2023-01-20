9ec3f229a227085fa584fcdcf706bd5d86f81277
付志强 <zhiqiangfu6@gmail.com>
2023-01-20 15:07:41 +0800

    feat: add parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,36 @@
+/**
+ * 
+ * @param tokens 
+ * @returns ast
+ */
+
 export function parser(tokens) {
-    return {}
+    return {
+        "type": "Program",
+        "children": [
+            {
+                "type": "Additive",
+                "children": [
+                    {
+                        "type": "Numeric",
+                        "value": "2"
+                    },
+                    {
+                        "type": "Multiplicative",
+                        "children": [
+                            {
+                                "type": "Numeric",
+                                "value": "3"
+                            },
+                            {
+                                "type": "Numeric",
+                                "value": "4"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
 
 }
--- tests/parser.spec.ts
+++ tests/parser.spec.ts
@@ -1,8 +1,42 @@
 import { parser } from "../src/parser";
 describe("parser", () => {
   it("parser tokens to ast", () => {
-    const tokens = []
-    const ast = {}
-    expect(parser(tokens)).toEqual(ast);
+    const tokens = [
+      { type: 'NUMBER', value: '2' },
+      { type: 'PLUS', value: '+' },
+      { type: 'NUMBER', value: '3' },
+      { type: 'MULTIPLY', value: '*' },
+      { type: 'NUMBER', value: '4' }
+    ]
+
+    const ast = {
+      "type": "Program",
+      "children": [
+        {
+          "type": "Additive",
+          "children": [
+            {
+              "type": "Numeric",
+              "value": "2"
+            },
+            {
+              "type": "Multiplicative",
+              "children": [
+                {
+                  "type": "Numeric",
+                  "value": "3"
+                },
+                {
+                  "type": "Numeric",
+                  "value": "4"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+    const res = parser(tokens)
+    expect(parser(res)).toEqual(ast);
   });
 });
